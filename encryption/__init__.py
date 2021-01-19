01663b0f71a4ac18d09e9aac8fc6846da38ceeb5
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-01-19 21:02:11 +0200

    feat: read write files

---
+++ encryption/__init__.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+Path("output").mkdir(parents=True, exist_ok=True)
--- encryption/encryption.py
+++ encryption/encryption.py
@@ -1,2 +1,10 @@
+OUTPUT_FOLDER = 'output/'
+
+
 def main(inputPath, outputPath):
-    print(f"input {inputPath}, output: {outputPath}")
+    with open(inputPath, 'r') as inputFile:
+        input = inputFile.read()
+    print(f'input: {input}')
+    result = "output"
+    with open(OUTPUT_FOLDER + outputPath, 'w') as outputFile:
+        outputFile.write(result)
